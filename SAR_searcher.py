adc7e53e5832075833d1a5825412630bd2ace7ba
Jose Culla <joseculla@outlook.com>
2019-04-13 19:25:18 +0200

    added main, basic non implemented functions and argument parsing

--- SAR_searcher.py
+++ SAR_searcher.py
@@ -7,6 +7,36 @@ Pablo López Orrios
 Jose Antonio Culla de Moya 
 '''
 
+import argparse
+
+def load_index(index_dir):
+    return 0
+
+def parse_query(query):
+    return 0
+
+def search(parses_query):
+    return 0
+
+def search_and_print(text):
+        parsed_query = parse_query(query)
+        search(parsed_query)
 
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("index", help="index directory")
+    parser.add_argument("-q", help="query to search")
+
+    args = parser.parse_args()
+
+    index = load_index(args.index)
+
+    if args.q:
+        search_and_print(args.q)
+    else:
+        while True:
+            query = raw_input("Introduzca una consulta: ")
+            if len(query) == 0:
+                break
+            search_and_print(query)
+
