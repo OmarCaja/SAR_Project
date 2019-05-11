bdcb3107ac95d5eacfa9088c034c0a07271ee1c5
Omar <omar.caja@hotmail.com>
2019-05-11 14:05:59 +0200

    Added a new method that gets json data for a given key

--- SAR_indexer.py
+++ SAR_indexer.py
@@ -14,9 +14,12 @@ import os
 import argparse
 import pickle
 import re
+import json
 
 doc_id = -1
 new_pos_in_doc = -1
+
+directory_path = '/'
 
 clean_re = re.compile('\\W+')
 
@@ -40,12 +43,28 @@ def get_new_pos_in_doc():
     return new_pos_in_doc
 
 
-def get_files_from_directory(directory):
+def get_json_data(filename):
+
+    with open(filename, "r") as json_file:
+        return json.load(json_file)
+
+
+def index_value_from_json(json_data, key):
+
+    for new in json_data:
+        print(new[key])
+        print(get_new_pos_in_doc())
+
+
+def index_files_from_directory(directory):
 
     for filename in os.listdir(directory):
 
         print(get_doc_id())
         print(filename)
+
+        json_data = get_json_data(directory + directory_path + filename)
+        index_value_from_json(json_data, 'article')
 
         global new_pos_in_doc
         new_pos_in_doc = -1
@@ -69,6 +88,6 @@ if __name__ == "__main__":
     index_name = args.index
     index = {}
 
-    get_files_from_directory(docs_directory)
+    index_files_from_directory(docs_directory)
 
     save_index(index, index_name)
