1d837e90e1cbe10c48a843145e671a9d6413e245
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2019-10-12 11:24:00 +0200

    añadido un getnumNodo en trie y arreglar pequeños fallos

--- trie.py
+++ trie.py
@@ -2,27 +2,33 @@ import trie_nodo as node
 
 class trie:
 
-    raiz = dict()
-    
+    def __init__(self):
+        self.raiz = dict()
+        self.numNodo = 0
+
     def addPalabra(self, palabra):
+        nodo = self.raiz.get(palabra[0],False)
+        if(not nodo):
+            self.numNodo += 1
+            nodo = node.trie_nodo(1, None, False)
+            self.raiz[palabra[0]] = nodo
         
-        nodo = node.trie_nodo(1, None, False)
-        self.raiz[palabra[0]] = nodo
+        
+        
         
         for letra in palabra[1:-1]:
-            nodo.anadirHijo(letra, False)
+            self.numNodo += nodo.anadirHijo(letra, False)
             nodo = nodo.recuperarNodo(letra)
 
         if (len(palabra) > 1):
-            nodo.anadirHijo(palabra[-1], True)
+            self.numNodo += nodo.anadirHijo(palabra[-1], True)
         else:
             nodo.final = True
-
 
     def existePalabra(self, palabra):
 
         if (len(palabra) != 0):
-                nodo = self.raiz.get(palabra[0], False)
+            nodo = self.raiz.get(palabra[0], False)
             if(not nodo):
                 return False
             for letra in palabra[1:]:
@@ -32,6 +38,9 @@ class trie:
             return nodo.final
         else:
             return True
+    
+    def getNumNodo(self):
+        return self.numNodo
                 
 
 
--- trie_nodo.py
+++ trie_nodo.py
@@ -1,16 +1,25 @@
 
 class trie_nodo:
 
-    hijos = dict()
+    
 
     def __init__(self, indice, nodo_padre, final):
         self.indice = indice
         self.nodo_padre = nodo_padre
         self.final = final
+        self.hijos = dict()
 
 
     def anadirHijo(self, letra, esPalabra):
-        self.hijos[letra] = trie_nodo(self.indice + 1, self, esPalabra)
+        nodo = self.hijos.get(letra,False)
+        if(not nodo):
+            self.hijos[letra] = trie_nodo(self.indice + 1, self, esPalabra)
+            return 1
+        
+        nodo.final = esPalabra
+        return 0
+
+        
 
     def recuperarNodo(self, letra):
         return self.hijos[letra]
