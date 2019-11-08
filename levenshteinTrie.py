0d2c7c5dde36a7e676385b355945475db8e07983
CULLA DE MOYA <joculde@EVIRL-011-OK.dsic.cloud>
2019-11-08 20:33:06 +0100

    completado trie damerau

--- levenshteinTrie.py
+++ levenshteinTrie.py
@@ -53,43 +53,56 @@ def levenshteinTrie(p,t):
 
 
 
-    def damareu_levenshteinTrie(p,t):
-        lenpalabra = len(p)
-        numNodo = t.getNumNodo()
-        nodos = t.getAllNode()
-        dis= np.empty(dtype = np.int8,shape=(lenpalabra+1,numNodo+1))
-        res = list()
-            
-        for i in range(lenpalabra+1):esPalabra
-            dis[i][0] = i
-            for n in nodos:
-                if(i == 0):
-                    dis[i][n.indice] = n.profundidad
-                else:
-                    disMin = dis[i][n.indice] + 1
+def damareu_levenshteinTrie(p,t):
+    lenpalabra = len(p)
+    numNodo = t.getNumNodo()
+    nodos = t.getAllNode()
+    dis= np.empty(dtype = np.int8,shape=(lenpalabra+1,numNodo+1))
+    res = list()
+        
+    for i in range(lenpalabra+1):
+        dis[i][0] = i
+        for n in nodos:
+            if(i == 0):
+                dis[i][n.indice] = n.profundidad
+            else:
+                
 
-                    if(n.nodo_padre):
-                        indicePadre = n.nodo_padre.indice
-                    else:
-                        indicePadre = 0
+                if(n.nodo_padre):
+                    indicePadre = n.nodo_padre.indice
+                else:
+                    indicePadre = 0
+
+
+                if(n.myKey == p[i-1]):
+                    disMin = dis[i-1][indicePadre]
+                else:
+                    disMin = dis[i-1][indicePadre]+1
+                
+                if(dis[i][indicePadre]+1 < disMin):
                     if(dis[i][indicePadre]+1 < disMin):
                         disMin = dis[i][indicePadre]+1
-                    if(dis[i-1][indicePadre] < disMin):
-                        disMin = dis[i-1][indicePadre]
-                        if(p[i-1] != n.myKey):
-                            disMin +=1
-                    if(dis[i-1][indicePadre] < disMin):
-                        disMin = dis[i-1][indicePadre]
-                        if(p[i-1] != n.myKey):
-                            disMin +=1
-                    dis[i][n.indice] = disMin
-                    if(i == lenpalabra and n.final):
-                        palabra = n.myKey
-                        padre = n.nodo_padre
-                        while padre :
-                            
-                            palabra = padre.myKey + palabra
-                            padre = padre.nodo_padre
-                        res.append((palabra,disMin))
-        
-        return res
+
+                if(dis[i-1][n.indice]+1 < disMin):
+                    if(dis[i-1][n.indice]+1 < disMin):
+                        disMin = dis[i-1][n.indice]+1
+                if i >= 2 and n.profundidad >= 2:
+
+                    if(n.myKey == p[i-2] and n.nodo_padre.myKey == p[i-1]):
+                        abuelo = n.nodo_padre.nodo_padre.indice
+                        if(dis[i-2][abuelo]+1 < disMin):
+                            disMin = dis[i-2][abuelo]+1
+
+
+                dis[i][n.indice] = disMin
+
+                if(i == lenpalabra and n.final):
+                    palabra = n.myKey
+                    padre = n.nodo_padre
+                    while padre :
+                        
+                        palabra = padre.myKey + palabra
+                        padre = padre.nodo_padre
+                    res.append((palabra,disMin))
+    
+    return res
