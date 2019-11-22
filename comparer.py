5b3e4057badcdbfe1ce678879db850d336680f31
Omar <omar.caja@hotmail.com>
2019-11-22 20:26:42 +0100

    Added word lists

--- comparer.py
+++ comparer.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
 
 import argparse
-from reader import get_word_list
+
 from Damerau_Leveshtein import damerau_levenshtein
+from reader import get_word_list
 
 
-def compare_word_list(word_list):
+def compare_word_list(word_list, searched_word, max_dist):
+    word_distance_list = make_word_distance_list(max_dist)
 
-    for word1 in word_list:
+    for word in word_list:
+        dist = damerau_levenshtein(searched_word, word)
+        if dist <= max_dist:
+            word_distance_list[dist].add((str(dist) + ':' + word))
 
-        print(word1)
-        print(' ')
+    print(word_distance_list)
+    print(len(word_distance_list[max_dist]))
 
-        for word2 in word_list:
 
-            print(word2)
-            print(' ')
-            print(damerau_levenshtein(word1, word2))
-        
-        print('\n')
+def make_word_distance_list(max_dist):
+    word_distance_list = []
+
+    for pos in range(max_dist + 1):
+        word_distance_list.append(set())
+
+    return word_distance_list
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
     parser.add_argument("file_name", help="File name")
+    parser.add_argument("word_and_distance", help="Word for comparing and max distance")
 
     args = parser.parse_args()
     file_name = args.file_name
+    searched_word = args.word_and_distance
 
     word_list = get_word_list(file_name)
-    compare_word_list(word_list)
+    compare_word_list(word_list, searched_word, 3)
--- reader.py
+++ reader.py
@@ -1,20 +1,17 @@
-#!/usr/bin/python3
-
 import re
 
 clean_re = re.compile('\\W+')
 
 
 def clean_text(text):
-
     return clean_re.sub(' ', text)
 
 
 def get_word_list(file_name):
-
     file = open(file_name, 'r')
     text = file.read()
     file.close()
 
-    cleaned_text = clean_text(text)
+    text_in_lower = text.lower()
+    cleaned_text = clean_text(text_in_lower)
     return cleaned_text.split()
