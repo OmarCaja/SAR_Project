b0dc8eb5410d0d9808c29375d5b3923035be2924
Omar <omar.caja@hotmail.com>
2019-11-25 20:41:48 +0100

    Added read_data package

--- read_data/reader.py
+++ read_data/reader.py
@@ -3,15 +3,15 @@ import re
 clean_re = re.compile('\\W+')
 
 
-def clean_text(text):
+def _clean_text(text):
     return clean_re.sub(' ', text)
 
 
-def get_word_list(file_name):
+def get_words_list(file_name):
     file = open(file_name, 'r')
     text = file.read()
     file.close()
 
     text_in_lower = text.lower()
-    cleaned_text = clean_text(text_in_lower)
+    cleaned_text = _clean_text(text_in_lower)
     return cleaned_text.split()
